9f6e103d67b86481b8507afdd9385fff51524f4c
Srirangan <srirangan@gmail.com>
2012-08-11 21:44:01 +0530

    String similarity based on Simon White's letter-pair method

---
+++ src/main/scala/LetterPairSimilarity.scala
@@ -0,0 +1,45 @@
+/**
+ * An implementation of a string similarity algorithm using the LetterPairSimilarity method.
+ * Based on Simon White's work at http://www.catalysoft.com/articles/StrikeAMatch.html
+ */
+
+object LetterPairSimilarity {
+
+  def compareStrings(string1: String, string2: String): Double = {
+    val pairs1: List[String] = wordLetterPairs(string1)
+    val pairs2: List[String] = wordLetterPairs(string2)
+    var intersection = 0
+    val union = pairs1.length + pairs2.length
+    pairs1.foreach((pair1: String) => {
+      pairs2.foreach((pair2: String) => {
+        if (pair1.toLowerCase == pair2.toLowerCase) {
+          intersection += 1
+          pairs2 diff pair2
+        }
+      })
+    })
+    (2.0 * intersection) / union
+  }
+
+  private def wordLetterPairs(string: String): List[String] = {
+    var allPairs: List[String] = List()
+    val words: Array[String] = string.split("\\s")
+    words.foreach((word: String) => {
+      val pairs: List[String] = letterPairs(word)
+      pairs.foreach((pair: String) => {
+        allPairs ::= pair
+      })
+    })
+    allPairs
+  }
+
+  private def letterPairs(string: String): List[String] = {
+    val numberOfPairs: Int = string.length - 1
+    var pairs: List[String] = List()
+    for (i <- 0 to numberOfPairs - 1) {
+      pairs ::= string.substring(i, i + 2)
+    }
+    pairs
+  }
+
+}
---
+++ src/test/scala/LetterPairSimilaritySuite.scala
@@ -0,0 +1,20 @@
+import org.scalatest.{Ignore, FunSuite, BeforeAndAfter}
+
+class LetterPairSimilaritySuite extends FunSuite with BeforeAndAfter {
+
+  test("'France' and 'Republic of France'") {
+    val score: Double = LetterPairSimilarity.compareStrings("France", "Republic of France")
+    assert(score === 0.5555555555555556)
+  }
+
+  test("'French Republic' and 'Republic of France'") {
+    val score: Double = LetterPairSimilarity.compareStrings("French Republic", "Republic of France")
+    assert(score === 0.8)
+  }
+
+  test("'French Republic' and 'Republic of Cuba'") {
+    val score: Double = LetterPairSimilarity.compareStrings("French Republic", "Republic of Cuba")
+    assert(score === 0.782608695652174)
+  }
+
+}
